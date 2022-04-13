cc3b69f1ab5b63a8f74fa31295882d5559657825
Simon Boman Laptop <simonb@live.se>
2022-04-13 18:30:28 +0200

    assignmen3 complete

--- lab1/lab1_ass3.R
+++ lab1/lab1_ass3.R
@@ -1,4 +1,4 @@
-###############################1a#####################################
+###############################3a#####################################
 
 #north is 0
 degrees = c(285, 296, 314, 20, 299, 296, 40, 303, 326, 308)
@@ -21,9 +21,33 @@ besselDegree = 0
 
 posterior = function(k, y, mju, degree, lambda) {
   #posterior proportional to this:
-  posterior = exp(k*cos(y-mju))/besselI(k, degree) * exp(-lambda*y)
+  #posterior = sum( exp(k*cos(y-mju))/besselI(k, degree) ) * lambda*exp(-lambda*k)
+  #
+  posterior = prod( exp(k*cos(y-mju))/besselI(k, degree) ) * dexp(k)
 }
 
  
 #gör om neg radians till positiva: 
-g = posterior(1,2, mju, besselDegree, 1)
+
+k = seq(0.1, 8, 0.005)
+g = posterior(0.1, radians, mju, besselDegree, lambda)
+
+xD = c(1:length(k))
+for(i in 1:length(k)) {
+  xD[i] = posterior(k[i], radians, mju, besselDegree, lambda)
+  
+}
+
+
+plot(k, xD, type="l")
+o
+xD = xD/sum(xD)
+
+plot(k, xD, type="l")
+
+
+###############################3b#####################################
+
+approx_post_K = k[which.max(xD)]
+approx_post_K
+#ctrl shift c
