dab31b5c7b583b56bbd9b2b5a448b791bb7c964d
Simon Boman Laptop <simonb@live.se>
2022-04-13 17:14:30 +0200

    assignment 2 finished. started assignment 3a

--- lab1/lab1_ass2.R
+++ lab1/lab1_ass2.R
@@ -8,7 +8,7 @@ tao = sum((log(data)-mju)^2)/n
 
 NDraws = 10000
 PostDraws <- c(1:NDraws)
-set.seed(12345)
+#set.seed(12345)
 PostDraws <- ((n)*tao)/rchisq(NDraws,n) 
 #PostDraws[,1] <- rlnorm(NDraws, mean=tao, sd=sqrt(PostDraws[,2]/n))
 
@@ -43,6 +43,30 @@ qnorm(0.975, mean, sd)
 density = density(G)
 density
 
-sortedG = sort(G)
-densitySorted = density(sortedG)
-densitySorted
+
+x = density$x
+y = density$y
+xy = cbind(x,y)
+
+xy_sorted = xy[order(xy[,2],decreasing=TRUE),]
+
+#get index of 95% mass
+cumSum = cumsum(xy_sorted[,2])
+stop = sum(xy_sorted[,2])*0.95
+index = min(which(cumSum > stop)) - 1
+
+xy_sorted_95 = xy_sorted[1:index,]
+
+lower = which.min(xy_sorted_95[,1])
+upper = which.max(xy_sorted_95[,1])
+lower
+upper
+
+lower = xy_sorted_95[lower,1]
+upper = xy_sorted_95[upper,1]
+lower
+upper
+
+abline(v=lower, col="blue")
+abline(v=upper, col="blue")
+
--- lab1/lab1_ass3.R
+++ lab1/lab1_ass3.R
@@ -1,3 +1,29 @@
 ###############################1a#####################################
 
+#north is 0
 degrees = c(285, 296, 314, 20, 299, 296, 40, 303, 326, 308)
+radians = c(1.83, 2.02, 2.33, -2.79, 2.07, 2.02, -2.44, 2.14, 2.54, 2.23)
+
+#?Bessel #mju - mean direcction, K concentration parameter
+#Large k -> small variance around mju, and vice versa. 
+
+
+mju = 2.51
+lambda = 1
+besselDegree = 0
+
+
+#posterior proportioanl to likelihood * prior
+#likelihood proportional to exp(k*cos(y-mju)) / I0(k), mju=2.51
+#K~Exp(lambda=1) which gives the prior: 
+#prior = lambda*exp(-lambda*y), lambda = 1
+
+
+posterior = function(k, y, mju, degree, lambda) {
+  #posterior proportional to this:
+  posterior = exp(k*cos(y-mju))/besselI(k, degree) * exp(-lambda*y)
+}
+
+ 
+#gör om neg radians till positiva: 
+g = posterior(1,2, mju, besselDegree, 1)
