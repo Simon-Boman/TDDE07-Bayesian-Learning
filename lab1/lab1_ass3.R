3851adbe106ce0b6627b4ef2f84c17d48307a9f5
Simon Boman Laptop <simonb@live.se>
2022-04-18 12:55:48 +0200

    cleaned up and added comments in the code for lab1 assignemnt 1,2,3

--- lab1/lab1_ass3.R
+++ lab1/lab1_ass3.R
@@ -2,50 +2,42 @@
 degrees = c(285, 296, 314, 20, 299, 296, 40, 303, 326, 308)
 radians = c(1.83, 2.02, 2.33, -2.79, 2.07, 2.02, -2.44, 2.14, 2.54, 2.23)
 
-#?Bessel #mju - mean direcction, K concentration parameter
-#Large k -> small variance around mju, and vice versa. 
-
-
 mju = 2.51
 lambda = 1
 besselDegree = 0
 
-
-#posterior proportioanl to likelihood * prior
-#likelihood proportional to exp(k*cos(y-mju)) / I0(k), mju=2.51
-#K~Exp(lambda=1) which gives the prior: 
-#prior = lambda*exp(-lambda*y), lambda = 1
-
-
+#posterior proportional to likelihood * prior
+#likelihood proportional to exp(k*cos(y-mju)) / I0(k)
+#K~Exp(lambda=1) which gives the prior: lambda*exp(-lambda*k)
 posterior = function(k, y, mju, degree, lambda) {
-  #posterior proportional to this:
-  #posterior = sum( exp(k*cos(y-mju))/besselI(k, degree) ) * lambda*exp(-lambda*k)
-  #
-  posterior = prod( exp(k*cos(y-mju))/besselI(k, degree) ) * dexp(k)
+  #posterior proportional to:
+  posterior = prod( exp(k*cos(y-mju))/besselI(k, degree) ) * lambda*exp(-lambda*k)
+  #posterior = prod( exp(k*cos(y-mju))/besselI(k, degree) ) * dexp(k, rate=lambda)
 }
 
- 
-#gör om neg radians till positiva: 
+#grid of k-values to plot over
+gridstep = 0.005
+k = seq(0, 7, gridstep)
 
-k = seq(0.1, 8, 0.005)
-g = posterior(0.1, radians, mju, besselDegree, lambda)
-
-xD = c(1:length(k))
+#calculate and store the value for each of the k-values
+post = c(1:length(k))
 for(i in 1:length(k)) {
-  xD[i] = posterior(k[i], radians, mju, besselDegree, lambda)
-  
+  post[i] = posterior(k[i], radians, mju, besselDegree, lambda)
 }
 
+#normalizing the posterior distirbution of k so that it integrates to 1
+post = post/(sum(post)*gridstep)
+plot(k, post, type="l", main="Posterior distribution of k", col="blue")
 
-plot(k, xD, type="l")
-o
-xD = xD/sum(xD)
 
-plot(k, xD, type="l")
 
 
 ###############################3b#####################################
-
-approx_post_K = k[which.max(xD)]
+#the approximate posterior mode of k is the largest value in the posterior
+approx_post_K = k[which.max(post)]
 approx_post_K
-#ctrl shift c
+plot(k, post, type="l", main="Posterior distribution of k", col="blue")
+abline(v=approx_post_K, col="red")
+
+
+
