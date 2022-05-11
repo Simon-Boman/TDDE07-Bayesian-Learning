8a7c4e848e7a70c6c3ea3daa6ad3340ee0d7af83
SimonBomanDesktop <simonb@live.se>
2022-05-11 17:02:46 +0200

    lab3 assignment3a finished

--- lab3/lab3_ass3.R
+++ lab3/lab3_ass3.R
@@ -41,5 +41,22 @@ plot(xSimulations[20,], type="l", ylab = "x", main = "Traceplot, phi = 0.9")
 plot(xSimulations[21,], type="l", ylab = "x", main = "Traceplot, phi = 1")
 
 
+par(mfrow=c(3,3))
+plot(xSimulations[1,], type="l", ylab = "x", main = "Traceplot, phi = -1")
+for (i in 1:(length(phiVals)/3)) {
+  print(3*i)
+  plot(xSimulations[3*i,], type="l", ylab = "x", main = paste("Traceplot, phi = ", phiVals[3*i]))
+  
+}
+#plot(..., main=paste("Jaccard vs. Tsim for depths",  min.depth, "to",max.depth,"m", sep=" "))
+
 
 ######3b
+# Use your function from a) to simulate two AR(1)-processes, x1:T with phi = 0.2 and y1:T with phi = 0.95.
+# Now, treat your simulated vectors as synthetic data, and treat the values of mu, phi and sigma^2 as unknown parameters.
+# Implement Stan-code that samples from the posterior of the three parameters, using suitable non-informative priors of your choice.
+# [Hint: Look at the time-series models examples in the Stan user's guide/reference manual, 
+# and note the different parameterization used here.]
+
+
+
