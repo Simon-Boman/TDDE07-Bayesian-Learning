11670cad72be63b732e1df1ee7cd69d2f770a242
SimonBomanDesktop <simonb@live.se>
2022-05-11 18:09:02 +0200

    lab3 assignment3 a) finished

--- lab3/lab3_ass3.R
+++ lab3/lab3_ass3.R
@@ -48,15 +48,67 @@ for (i in 1:(length(phiVals)/3)) {
   plot(xSimulations[3*i,], type="l", ylab = "x", main = paste("Traceplot, phi = ", phiVals[3*i]))
   
 }
-#plot(..., main=paste("Jaccard vs. Tsim for depths",  min.depth, "to",max.depth,"m", sep=" "))
 
 
 ######3b
 # Use your function from a) to simulate two AR(1)-processes, x1:T with phi = 0.2 and y1:T with phi = 0.95.
 # Now, treat your simulated vectors as synthetic data, and treat the values of mu, phi and sigma^2 as unknown parameters.
+
 # Implement Stan-code that samples from the posterior of the three parameters, using suitable non-informative priors of your choice.
 # [Hint: Look at the time-series models examples in the Stan user's guide/reference manual, 
 # and note the different parameterization used here.]
 
+# i. Report the posterior mean, 95% credible intervals,
+# and the number of effective posterior samples for the three inferred parameters for each of the simulated AR(1)-process.
+# Are you able to estimate the true values? 
+
+# ii. For each of the two data sets, evaluate the convergence of the samplers and plot the joint posterior of mu and phi. Comments?
 
 
+
+StanModel = '
+data { 
+  int<lower=0> N; 
+  vector[N] y;    
+}
+parameters {
+  real mu;
+  real phi;
+  real<lower=0> sigma;
+}
+model {
+  for (n in 2:N)
+    y[n] ~ normal(mu + phi * (y[n-1] - mu) , sigma);
+}
+'
+
+##############phi = 0.2
+x = ARSim(0.2)
+
+data_x = list(N=T, y=x)
+#default for warmup and iter, default chains = 4
+fit_x = stan(model_code = StanModel, data=data_x)
+
+
+# Print the fitted model
+print(fit_x,digits_summary=3)
+
+# Extract posterior samples
+postDraws <- extract(fit_x)
+
+# Do traceplots of the first chain
+par(mfrow = c(1,1))
+plot(postDraws$mu, type="l",ylab="mu",main="Traceplot")
+
+# Do automatic traceplots of all chains
+traceplot(fit_x)
+
+# Bivariate posterior plots
+pairs(fit_x)
+
+
+########## phi = 0.95
+y = ARSim(0.95)
+
+data_y = list(N=T, y=y)
+fit_y =stan(model_code = StanModel, data=data_y)
